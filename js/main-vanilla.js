0a1f5163a116d96257f7909b1b45675efc8ef4a0
Menn <info@mennstudio.com>
2019-08-08 16:01:09 +0700

    Fix Mobile nav to show sub nav

--- js/main-vanilla.js
+++ js/main-vanilla.js
@@ -1,23 +1,43 @@
-// Mobile Menu
-document.querySelectorAll(".site-toggle").forEach(e => {
-    e.addEventListener("click", function() {
-        if (e.classList.contains("active")) {
-            removeClass(".site-toggle", "active");
-            removeClass(".site-nav-m", "active");
-            removeClass("#page", "show-nav");
-        } else {
-            addClass(".site-toggle", "active");
-            addClass(".site-nav-m", "active");
-            addClass("#page", "show-nav");
+// Using event delegation https://gomakethings.com/why-event-delegation-is-a-better-way-to-listen-for-events-in-vanilla-js/#web-performance
+document.addEventListener(
+    "click",
+    function(event) {
+        // Mobile Menu Toggle
+        if (event.target.matches(".site-toggle")) {
+            if (event.target.classList.contains("active")) {
+                removeClass(".site-toggle, .site-nav-m", "active");
+                removeClass("#page", "show-nav");
+            } else {
+                addClass(".site-toggle, .site-nav-m", "active");
+                addClass("#page", "show-nav");
+            }
         }
-    });
-});
-// Close menu on click (useful for One Page Website)
-document.querySelectorAll(".site-nav-m a").forEach(e => {
-    e.addEventListener("click", function() {
-        removeClass(".site-toggle", "active");
-        removeClass(".site-nav-m", "active");
-    });
+        if (event.target.matches("#site-nav-m .menu-item-has-children i")) {
+            if (event.target.parentNode.classList.contains("active")) {
+                event.target.parentNode.classList.remove("active");
+            } else {
+                event.target.parentNode.classList.add("active");
+            }
+        }
+        // Close menu on click (useful for One Page Website)
+        if (event.target.matches("#site-nav-m a")) {
+            if (event.target.getAttribute("href") == "#") {
+                if (event.target.parentNode.classList.contains("active")) {
+                    event.target.parentNode.classList.remove("active");
+                } else {
+                    event.target.parentNode.classList.add("active");
+                }
+            } else {
+                removeClass(".site-toggle, .site-nav-m", "active");
+            }
+        }
+    },
+    false
+);
+
+// Mobile Menu - Add Dropdown Toggle
+document.querySelectorAll("#site-nav-m .menu-item-has-children").forEach(e => {
+    e.insertAdjacentHTML("beforeend", '<i class="si-caret-down"></i>');
 });
 
 // Slider
