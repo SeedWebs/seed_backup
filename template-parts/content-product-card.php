fcaefeb4704a3309e47bddd7a44fc2a66371d0da
Menn <info@mennstudio.com>
2019-10-09 21:33:18 +0700

    temporary fix

--- template-parts/content-product-card.php
+++ template-parts/content-product-card.php
@@ -2,44 +2,49 @@
 /**
  * Loop Name: Product Card
  */
-?>
-<?php
-/**
- * Template part for displaying WooCommerce Product.
- *
- * @package seed
- */
-
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 global $product; ?>
+<div <?php wc_product_class( 'content-item -card -product', $product ); ?>>
+    <?php
+	/**
+	 * Hook: woocommerce_before_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_open - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item' );
 
-<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>"
-    <?php post_class('seed-col'); ?>>
-    <div class="content-item -card -product">
-        <a href="<?php echo esc_url( get_permalink( $product->id ) ); ?>"
-            title="<?php echo esc_attr( $product->get_title() ); ?>">
-            <div class="pic"><?php echo $product->get_image('shop_catalog'); ?></div>
-            <div class="info">
-                <h2 itemprop="name" class="entry-title product-title"><?php echo $product->get_title(); ?></h2>
-                <?php if ( ! empty( $show_rating ) ) : ?>
-                <?php echo $product->get_rating_html(); ?>
-                <?php endif; ?>
+	/**
+	 * Hook: woocommerce_before_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_show_product_loop_sale_flash - 10
+	 * @hooked woocommerce_template_loop_product_thumbnail - 10
+	 */
+	do_action( 'woocommerce_before_shop_loop_item_title' );
 
-                <div itemprop="description" class="product-summary">
-                    <?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
-                </div>
+	/**
+	 * Hook: woocommerce_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_product_title - 10
+	 */
+	do_action( 'woocommerce_shop_loop_item_title' );
 
-                <div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
-                    <p class="price"><?php echo $product->get_price_html(); ?></p>
-                </div>
+	/**
+	 * Hook: woocommerce_after_shop_loop_item_title.
+	 *
+	 * @hooked woocommerce_template_loop_rating - 5
+	 * @hooked woocommerce_template_loop_price - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item_title' );
 
-                <meta itemprop="url" content="<?php the_permalink(); ?>" />
-            </div>
-            <!--info-->
-        </a>
-    </div>
+	/**
+	 * Hook: woocommerce_after_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_close - 5
+	 * @hooked woocommerce_template_loop_add_to_cart - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item' );
+	?>
 </div>
